9864cb957e33c26e369e89a243c401d56664c1e5
Melissa Illig <melissaillig@melissas-mbp.home>
2020-01-11 16:23:54 +0100

    add Script

---
+++ Assets/MyGame/Scripts/SimpleUserInterfaceScript.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleUserInterfaceScript : MonoBehaviour
+{
+
+        public InputField input;
+        public Button textButton;
+        public Text result;
+
+        public Text counterText;
+        public int counter = 0;
+
+        public void CopyText()
+        {
+                result.text = input.text;
+
+        }
+
+        public void Counter()
+        {
+        counter++;
+        counterText.text = counter.ToString();
+        }
+}
